60a31f3b0789d22f6e5ce0f4f7b3f05880951cf7
ogorbachenko <ogorbachenko@reinvently.com>
2018-06-10 16:54:19 +0300

    add signin

--- Food2day/ViewController.swift
+++ Food2day/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
-
+import GoogleSignIn
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let signInButton = GIDSignInButton()
+        signInButton.center = view.center
+        
+        view.addSubview(signInButton)
     }
 
     override func didReceiveMemoryWarning() {
